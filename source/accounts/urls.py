9bbcf6fd64f98e412a95d0ed3b152075f235ba61
Beknasar <680633@gmail.com>
2020-09-26 11:35:31 +0600

    Добавлен модель Product. Добавлен accounts.

---
+++ source/accounts/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView
+from accounts.views import RegisterView, UserDetailView, UserListView, UserChangeView, UserPasswordChangeView, \
+    BasketClearLogoutView
+
+app_name = 'accounts'
+
+urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', BasketClearLogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='create'),
+    path('<int:pk>/', UserDetailView.as_view(), name='detail'),
+    path('list/', UserListView.as_view(), name='list'),
+    path('<int:pk>/update/', UserChangeView.as_view(), name='change'),
+    path('<int:pk>/password_change', UserPasswordChangeView.as_view(), name='password_change'),
+]
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+DEFAULT_CATEGORY = 'other'
+CATEGORY_CHOICES = {
+    (DEFAULT_CATEGORY, 'Разное'),
+    ('food', "Еда"),
+    ('toys', 'Игрушки'),
+    ('tech', 'Бытовая техника'),
+    ('tools', 'Инструменты')
+}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=35, null=False, blank=False, verbose_name='Название')
+    category = models.CharField(max_length=20, default=DEFAULT_CATEGORY, choices=CATEGORY_CHOICES, verbose_name='Категория')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Описание')
+    picture = models.ImageField(null=True, blank=True, upload_to='product_pics', verbose_name='Картинка продукта')
+
+    def __str__(self):
+        return f'{self.name} -- {self.category}'
+
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
